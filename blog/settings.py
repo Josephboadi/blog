f0e9b704f1a7f73244ce98677974df433e29f07c
Josephboadi <boadijoseph151@gmail.com>
2020-03-26 16:58:41 +0100

    new update

--- blog/settings.py
+++ blog/settings.py
@@ -141,7 +141,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #Tinymce
 
